137e22db72850dd7df7958991e8423565c96a8d4
gift56 <efeasiughu@gmail.com>
2023-01-05 10:00:12 -0800

    working on project

--- main.js
+++ main.js
@@ -3,4 +3,16 @@ const quotes = [
     quote: `"I will Never Love again!😒"`,
     writer: `-mk Gift`,
   },
+  {
+    quote: `"True Love shouldn't be this complicated💖"`,
+    writer: `-Mk Mikel`,
+  },
+  {
+    quote: `"Make Money while you're Young💵"`,
+    writer: `-Money Man`,
+  },
+  {
+    quote: `"I will Never Love again!😒"`,
+    writer: `-mk Gift`,
+  },
 ];
